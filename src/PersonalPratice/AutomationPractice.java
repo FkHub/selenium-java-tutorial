b95153525a14f3381782526e1b5e1a85212e0ac1
Faizan Khan <Khanfaizan.fk27@gmail.com>
2025-09-14 22:23:57 +0530

    Added a method to get password from the screen

--- src/PersonalPratice/AutomationPractice.java
+++ src/PersonalPratice/AutomationPractice.java
@@ -1,5 +1,6 @@
 package PersonalPratice;
 import java.time.Duration;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,7 +9,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class AutomationPractice {
-
+	
+	private String String;
+	
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Faizan Khan\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
@@ -16,24 +19,35 @@ public class AutomationPractice {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Faizan Khan\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		driver.get("https://www.saucedemo.com/");
+		String password = gettingPassword(driver);
 		driver.findElement(By.id("user-name")).sendKeys("standard_user");
-		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("secret_sauce");
-		driver.findElement(By.cssSelector("input[class *='submit-button']")).click();	
-		Thread.sleep(5000);
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.cssSelector("input[class *='submit-button']")).click();
+		//driver.switchTo().alert().dismiss();
+		//System.out.println(driver.switchTo().alert().getText());
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("//div/div/button[@id='react-burger-menu-btn']")).click();	
 		driver.findElement(By.cssSelector("a[id='logout_sidebar_link']")).click();
 		
 		driver.navigate().to("http://www.rahulshettyacademy.com/AutomationPractice/");
 		driver.navigate().back();
 		driver.navigate().forward();
-		Thread.sleep(5000);
+		Thread.sleep(2000);
 		driver.close();
 		
-		
-		//driver.close();		
+			
 		
 
+	}
+	
+	public static  String gettingPassword(WebDriver driver) {
+		
+		driver.get("https://www.saucedemo.com/");
+		String passwordList = driver.findElement(By.cssSelector("div[class='login_password']")).getText();
+		String [] passwordArray = passwordList.split(":");
+		String password = passwordArray[1];
+		return password;
+		
 	}
 
 }
