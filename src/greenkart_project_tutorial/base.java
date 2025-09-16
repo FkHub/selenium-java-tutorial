7ea20c8fd05acf95d8acea0f6dc1dbc5c3306887
Faizan Khan <Khanfaizan.fk27@gmail.com>
2025-09-17 00:14:40 +0530

    Expanded the code to go to cart apply promo code and also moved code of add to cart as a method

--- src/greenkart_project_tutorial/base.java
+++ src/greenkart_project_tutorial/base.java
@@ -1,9 +1,11 @@
 package greenkart_project_tutorial;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.By;import org.openqa.selenium.By.ByCssSelector;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,9 +17,25 @@ public class base {
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Faizan Khan\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe" );
 		WebDriver driver = new ChromeDriver();
-		String[] itemsNeeded = {"Cucumber","Brocolli","Beetroot","Tomato"};
-		int j=0;
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		String[] itemsNeeded = {"Cucumber","Brocolli","Beetroot"};
+	
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		addItems(driver, itemsNeeded);
+		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+		driver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click(); 
+		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector("button.promoBtn")).click();
+	System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
+		
+	}
+	
+	
+	
+	public static void addItems(WebDriver driver, String[] itemsNeeded)
+	{
+		int j=0;
 		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
 		for(int i =0; i<products.size(); i++)
 		{
@@ -41,5 +59,5 @@ public class base {
 		
 
 	}
-
+	
 }
